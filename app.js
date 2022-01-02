96d1044525ffb1a543f048895a31c6f7fc5fe646
Yash <yashpandey1106@gmail.com>
2022-01-02 15:59:10 +0530

    Added session configuration and handled the errors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 //third party packages
 const express  = require('express');
-const csrf = require('csurf')
-
+const csrf = require('csurf');
+const expressSession = require('express-session');
 //included files
 const authRoutes = require('./routes/auth.route');
 const db = require('./data/database');
-const addCsrfTokenMiddleware = require('./middlewares/csrf-token')
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const createSessionConfig = require('./config/session');
 
 const app = express();
 
@@ -18,10 +20,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false})) //extended false to support regular form submission
 
+const sessionConfig = createSessionConfig()
+app.use(expressSession(sessionConfig))
+
 app.use(csrf()); //This will executed as function and return actual middleware
 app.use(addCsrfTokenMiddleware)
 
 app.use(authRoutes);
+app.use(errorHandlerMiddleware)
 
 db.connectToDatabase().then(function() {
     app.listen(3000);
